fbdff8bf21dde6d26995d77d82bb15224de887c4
thekayecode <thekayecode@gmail.com>
2025-07-05 07:40:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
-import os
+from src.routes import hello
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI on Render!"}
+app.include_router(hello.router)
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))  # Render uses PORT env var
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+@app.get("/")
+def root():
+    return {"message": "Hello from FastAPI on Render!"}
